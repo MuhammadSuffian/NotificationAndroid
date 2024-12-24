937b51685141240571839357d950b0261766e840
Muhammad Suffian Tafoor <112380672+MuhammadSuffian@users.noreply.github.com>
2024-12-24 10:16:38 +0500

    v1

--- app/src/main/java/com/example/notification/MainActivity.java
+++ app/src/main/java/com/example/notification/MainActivity.java
@@ -2,11 +2,14 @@ package com.example.notification;
 
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -24,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
     void initlization(){
         binding=ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        createChannel();
         binding.btnNotify.setOnClickListener(v->{
             if(binding.etInputMsg.getText().toString().trim().isEmpty()){
                 binding.etInputMsg.setError("Please enter message");
@@ -31,8 +35,20 @@ public class MainActivity extends AppCompatActivity {
                 Snackbar.make(binding.getRoot(),"Please enter message",Snackbar.LENGTH_SHORT).show();
                 return;
             }
+            else{
+                Intent i=new Intent(this,MainActivity2.class);
+                PendingIntent p1=PendingIntent.getActivity(this,0,i,PendingIntent.FLAG_IMMUTABLE);
+                NotificationCompat.Builder builder=new NotificationCompat.Builder(this,"MyChannel")
+                        .setSmallIcon(R.drawable.ic_launcher_background)
+                        .setContentTitle("Lumber 1 Notification")
+                        .setContentText(binding.etInputMsg.getText().toString())
+                        .setContentIntent(p1).setAutoCancel(true);
+                NotificationManager manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+                manager.notify(1,builder.build());
+            }
         });
     }
+
     public void createChannel(){
         if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.O){
             String channelId="MyChannel";
